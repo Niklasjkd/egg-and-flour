95aae9e3396e30708043067fcadb5388765ff7c2
Hans-Conradt v. Wedemeyer <hc@vonwedemeyer.com>
2019-08-27 12:15:05 +0200

    added img, lng, lat to user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,22 @@ user = User.new(
     email: "text@example.com",
     first_name: "text",
     last_name: "example",
-    password: "123456"
+    password: "123456",
+    image: "https://images.unsplash.com/photo-1556911220-dabc1f02913a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+    lng:  52.520008,
+    lat:  13.404954
+
+  )
+user.save!
+
+user = User.new(
+    email: "text@example.com",
+    first_name: "text",
+    last_name: "example",
+    password: "123456",
+    image: "https://images.unsplash.com/photo-1556911220-e15b29be8c8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" ,
+    lng: 52.520010,
+    lat: 13.404500
   )
 user.save!
 
