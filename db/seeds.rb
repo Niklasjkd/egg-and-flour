aacf263b5c48b6de2ff266fb1a6ecf1b2a4117f3
Hans-Conradt v. Wedemeyer <hc@vonwedemeyer.com>
2019-08-27 12:04:46 +0200

    3 recipes with ingredients

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,122 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning databases...'
+# Review.destroy_all
+# Booking.destroy_all
+# Plane.destroy_all
+User.destroy_all
+
+puts 'Creating user...'
+
+user = User.new(
+    email: "text@example.com",
+    first_name: "text",
+    last_name: "example",
+    password: "123456"
+  )
+user.save!
+
+puts 'Creating recipes...'
+
+recipes = [ {
+    name: "Spanish tortilla",
+    description: "A Spanish classic",
+    category: "Spanish" ,
+    method: "Peel the potatoes using a speed-peeler, then carefully cut them into thin slices. Pat the potato slices dry with a clean tea towel.
+Peel and finely slice the onion. Drizzle 2 tablespoons of oil into a small frying pan over a medium heat, then add the onion and potatoes.
+Turn the heat down to low and cook for 25 to 30 minutes, or until the onions are turning golden and the potato slices are cooked through. Try not to stir it too much or the potatoes will break up – just use a fish slice to flip them over halfway through.
+Crack the eggs into a mixing bowl, season with a tiny pinch of sea salt and black pepper, then whisk together with a fork.
+When the onions and potatoes are cooked, remove the pan from the heat and carefully tip them into the eggs. Transfer the mixture back into the frying pan and place it over a low heat. Cook for around 20 minutes, or until there’s almost no runny egg on top.
+Use a fish slice to slightly lift and loosen the sides of the tortilla. Carefully flip the pan over a dinner plate and tip out the tortilla, then slide it back into the pan and cook for another 5 minutes, or until golden and cooked through.
+Turn out the tortilla onto a serving board, then cut into 6 wedges and serve hot or cold with a simple green salad. ",
+    image: "https://img.jamieoliver.com/jamieoliver/recipe-database/xtra_med/46260004.jpg?tr=w-430"
+  },{
+    name: "Mexican baked eggs",
+    description: "With zincy fresh lime" ,
+    category: "Mexican" ,
+    method:  "Preheat the oven to full whack.
+Grease a small skillet pan or round baking dish with a drizzle of olive oil, then crack in the eggs.
+Finely slice the red chilli (deseed if you like) and scatter over the eggs.
+Peel and destone the avocado, then slice. Halve the lime and squeeze over the juice of one half.
+Arrange the avocado around the eggs, and season with a little sea salt and black pepper.
+Place in the oven for 7 to 10 minutes, or until the whites are set but the yolks are still runny.
+Pick and roughly chop a few coriander leaves and sprinkle over the eggs, then cut the remaining lime half into wedges for squeezing over. Delicious served with hot buttered toast.",
+    image: "https://img.jamieoliver.com/jamieoliver/recipe-database/xtra_med/90303516.jpg?tr=w-430"
+  },{
+    name: "Avocado on rye toast with ricotta",
+    description: "Tomato, fresh basil & toasted pine nuts" ,
+    category: "Vegetarian" ,
+    method:  "Spread the ricotta cheese over the rye bread or toast.
+Finely slice the avocado and tomato, then toss with a squeeze of lemon juice.
+Season to taste and arrange on the toast.
+Sprinkle over the pine nuts and a few fresh baby basil leaves, if you’ve got them.",
+    image: "https://img.jamieoliver.com/jamieoliver/recipe-database/xtra_med/46264954.jpg?tr=w-430"
+  }]
+recipes.each { |recipe| Recipe.new(recipe).save! }
+
+
+puts 'Creating ingredients...'
+
+ingredients = [{
+  name: "waxy potatoes",
+  unit: "grammes"
+}, {
+  name: "lemon"
+}, {
+  name: "rye bread",
+  unit: "75g slice"
+},{
+  name: "pine nuts",
+  unit: "teaspoon"
+}, {
+  name: "tomato"
+}, {
+  name: "lime"
+}, {
+  name: "avocado"
+}, {
+  name: "red chilli"
+}, {
+  name: "Onion"
+}, {
+  name: "Eggs"
+}]
+
+ingredients.each { |ingredient| Ingredient.new(ingredient).save!}
+
+
+recipe_ingredients = [{
+  recipe_id: Recipe.where(name: "Spanish tortilla").first.id,
+  quantity: 300,
+  ingredient_id: Ingredient.where(name: "waxy potatoes").first.id
+}, {
+  recipe_id: Recipe.where(name: "Spanish tortilla").first.id,
+  quantity: 1,
+  ingredient_id: Ingredient.where(name: "Onion").first.id
+}, {
+  recipe_id: Recipe.where(name: "Spanish tortilla").first.id,
+  quantity: 5,
+  ingredient_id: Ingredient.where(name:"Eggs").first.id
+},{
+  recipe_id: Recipe.where(name: "Mexican baked eggs").first.id,
+  quantity: 4,
+  ingredient_id: Ingredient.where(name: "Eggs").first.id
+},{
+  recipe_id: Recipe.where(name: "Mexican baked eggs").first.id,
+  quantity: 1,
+  ingredient_id: Ingredient.where(name: "red chilli").first.id
+},{
+  recipe_id: Recipe.where(name: "Mexican baked eggs").first.id,
+  quantity: 1,
+  ingredient_id: Ingredient.where(name: "avocado").first.id
+},{
+  recipe_id: Recipe.where(name: "Mexican baked eggs").first.id,
+  quantity: 1,
+  ingredient_id: Ingredient.where(name: "lime").first.id
+},
+]
+
+recipe_ingredients.each { |recipe_ingredient| RecipeIngredient.new(recipe_ingredient).save! }
+puts 'Finished!'
