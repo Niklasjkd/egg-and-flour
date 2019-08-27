c93208543febcc5e71512f75715b1cc38df3dba0
Angelo Ruperti <arruperti@gmail.com>
2019-08-27 16:50:09 +0200

    added javascript function to select ids of recipe

---
+++ app/javascript/components/init_select.js
@@ -0,0 +1,24 @@
+
+
+
+
+const recipes = document.querySelectorAll(".recipe")
+let recipeArray = [];
+
+const highlight = () => {
+    recipes.forEach(recipe => {
+      recipe.addEventListener('click', (event) => {
+        event.target.classList.toggle("highlight")
+        if(recipeArray.includes(recipe.dataset.id)) {
+          recipeArray = [...recipeArray].filter(val => val !== recipe.dataset.id)
+        } else {
+          recipeArray.push(recipe.dataset.id);
+
+        }
+          console.log(recipeArray);
+    })
+  });
+  return recipeArray;
+};
+
+export { highlight };
