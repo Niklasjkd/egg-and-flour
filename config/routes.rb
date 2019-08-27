f5bde55faf3d10be7b09e7a782bcd3563a88d1f5
Angelo Ruperti <arruperti@gmail.com>
2019-08-27 11:04:31 +0200

    better style and fix syntax

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'recipes/index'
   devise_for :users
   root to: 'pages#home'
-  resources :recipes, only: [show:, index:]
+  resources :recipes, only: [:show, :index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
