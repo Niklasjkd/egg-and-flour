682aa74b4e7439bc6d7fe4a79eb6d5bcd470c7d3
Hans-Conradt v. Wedemeyer <hc@vonwedemeyer.com>
2019-08-28 16:16:29 +0200

    create request function

--- app/javascript/components/init_select.js
+++ app/javascript/components/init_select.js
@@ -1,18 +1,25 @@
 const initHighlight = () => {
   const recipes = document.querySelectorAll(".recipe")
   let recipeArray = [];
+  let requests_recipes = document.querySelector('#requests_recipes');
     recipes.forEach(recipe => {
       recipe.addEventListener('click', (event) => {
         event.target.classList.toggle("highlight")
         if(recipeArray.includes(recipe.dataset.id)) {
-          recipeArray = [...recipeArray].filter(val => val !== recipe.dataset.id)
+          recipeArray = [...recipeArray].filter(val => val !== recipe.dataset.id);
+          console.log(recipeArray);
+          requests_recipes.value = recipeArray;
+          console.log(requests_recipes);
         } else {
           recipeArray.push(recipe.dataset.id);
-
+          console.log(recipeArray);
+          requests_recipes.value = recipeArray;
+          console.log(requests_recipes);
         }
     })
   });
-  return recipeArray;
 };
+// QUERY THE input (look at the id)
+// update value of input with recipeArray
 
 export default initHighlight;
