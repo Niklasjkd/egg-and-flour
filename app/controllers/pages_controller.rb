7b9cb2113a588046df6588414031d86de7ba6620
Niklas Düttmann <niklas.duettmann033@gmail.com>
2019-08-29 11:30:21 +0200

    css for pop

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,21 +9,5 @@ class PagesController < ApplicationController
       ingredients = Ingredient.where(category: category).order('name ASC').map { |ingredient| ingredient.name }.uniq
       @ingredient_types << { title: category, image: "landing/#{category}.png", ingredients: ingredients }
     end
-
-    # @ingredients = ["Sugar", "Milk", "Eggs", "Cheese"]
-
-    # @ingredient_types = [
-    #   { title: "Dairy", image: "landing/dairy.png", ingredients: @ingredients },
-    #   { title: "Desserts & Snacks", image: "landing/desserts-snacks.png", ingredients: @ingredients },
-    #   { title: "Fish", image: "landing/fish.png", ingredients: @ingredients },
-    #   { title: "Legumes", image: "landing/legumes.png", ingredients: @ingredients },
-    #   { title: "Meats", image: "landing/meats.png", ingredients: @ingredients },
-    #   { title: "Nuts", image: "landing/nuts.png", ingredients: @ingredients },
-    #   { title: "Oils", image: "landing/oils.png", ingredients: @ingredients },
-    #   { title: "Sauces", image: "landing/sauces.png", ingredients: @ingredients },
-    #   { title: "Seafood", image: "landing/seafood.png", ingredients: @ingredients },
-    #   { title: "Seasonings", image: "landing/seasonings.png", ingredients: @ingredients },
-    #   { title: "Fruits", image: "landing/fruits.png", ingredients: @ingredients }
-    # ]
   end
 end
