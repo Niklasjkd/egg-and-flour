d5b4b79ea5bb9acc3610f5004ef3bcf366a1e8db
Donatus Salsali <donatus@salsali.cc>
2019-09-02 14:02:53 +0200

    api key

--- app/javascript/components/init_select.js
+++ app/javascript/components/init_select.js
@@ -3,8 +3,8 @@ const displayRecipes = document.querySelector("#display-recipes");
 
 const initDisplay = () => {
   results.forEach((result) => {
-    fetch(`https://www.food2fork.com/api/search?key=e3dd7319146611f94f40d742afb393ec&q=${result.innerText}`)
-    
+    fetch(`https://www.food2fork.com/api/search?key=f30d2e2fc20ed1e6fad332c7ca8c38e8&q=${result.innerText}`)
+
     .then(response => response.json())
     .then(({recipes}) => {
       recipes.slice(0, 5).forEach((r) => {
