9aaaaf8ab68887bfa79c82fbbdc7d9ee4ba96a75
Niklas Düttmann <niklas.duettmann033@gmail.com>
2019-09-04 11:54:53 +0200

    added sticky btn to recipe index

--- app/javascript/components/init_recipe_popover.js
+++ app/javascript/components/init_recipe_popover.js
@@ -70,6 +70,12 @@ function clickSelect() {
   requests_recipes.value = JSON.stringify(recipes_arr);
   recipeCard.classList.add("highlight");
   clickClose();
+
+  var recipeName = [];
+  recipes_arr.forEach(function(r) {
+    recipeName.push(r["title"]);
+  });
+  updateBtnRecipeNamesArr(recipeName);
 }
 
 function initClickForPopover() {
@@ -87,5 +93,7 @@ function initClickForPopover() {
     });
   }
 }
+
+import { updateBtnRecipeNamesArr } from "../components/init_recipe_btn";
 
 export { initClickForPopover };
