398a05e39d7c9c9454c269afa0c2469c97213e08
Angelo Ruperti <arruperti@gmail.com>
2019-09-04 11:32:10 +0200

    domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { host: ENV["DOMAIN"] || "localhost:3000" }
+    { host: ENV["DOMAIN"] || "www.eggsandflour.co" }
   end
 
   private
