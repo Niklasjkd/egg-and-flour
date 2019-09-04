0cdf650bbaaef04c02930226edd1937c576453a7
Hans-Conradt v. Wedemeyer <hc@vonwedemeyer.com>
2019-09-04 13:40:08 +0200

    reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,6 +7,7 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new(review_params)
     @review.save!
+
     redirect_to dashboard_profile_path(current_user)
   end
 
