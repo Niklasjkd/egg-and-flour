7891b2767a5a56d46b34b331cc06caff4d14e486
Hans-Conradt v. Wedemeyer <hc@vonwedemeyer.com>
2019-09-05 13:14:44 +0200

    reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
-  belongs_to :meetup
-  belongs_to :user
+  # belongs_to :meetup
+  # belongs_to :user
   validates :content, presence: true
   validates :rating, presence: true
 end
